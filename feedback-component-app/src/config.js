58afc315495c07109b5c026d3c8d421cf1610e43
George <psygh3@nottingham.ac.uk>
2019-02-03 19:57:18 +0000

    Added the feedback component back in
    currently broken: Doesn't display the antd components with normal styling.

---
+++ feedback-component-app/src/config.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://feedback-analysis-grp-app.herokuapp.com/";
+export const appUrl = "https://feedback-analysis.herokuapp.com/";
+// export const corsHelper = "https://cors-anywhere.herokuapp.com/";
