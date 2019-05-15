9cb18a79fb0be029998397a0e06826bfe466c224
Songkeys <zy22132@nottingham.edu.cn>
2019-05-15 08:50:43 +0100

    Larger font size on screen

--- demo/src/index.js
+++ demo/src/index.js
@@ -45,35 +45,42 @@ class Demo extends Component {
 
   render() {
     return (
-      <div style={{ padding: 20 }}>
-        <h1>feedback-component Demo</h1>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '1.5em',
+      }}>
+        <div style={{ padding: 20, width: '50vw' }}>
+          <h1>feedback-component Demo</h1>
 
-        <p>This is a demo website for the feedback-component.</p>
+          <p>This is a demo website for the feedback-component.</p>
 
-        <p>Select your dashboard:</p>
+          <p>Select your dashboard:</p>
 
-        <Select
-          loading={this.state.isLoading}
-          showSearch
-          style={{ width: 200 }}
-          placeholder="Select a dashboard..."
-          onChange={this.handleDashboardChange}
-        >
-          {this.state.dashboardList.map(d => (
-            <Select.Option key={d.id} value={d.id}>
-              {d.name}
-            </Select.Option>
-          ))}
-        </Select>
+          <Select
+            loading={this.state.isLoading}
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Select a dashboard..."
+            onChange={this.handleDashboardChange}
+          >
+            {this.state.dashboardList.map(d => (
+              <Select.Option key={d.id} value={d.id}>
+                {d.name}
+              </Select.Option>
+            ))}
+          </Select>
 
-        <p />
+          <p />
 
-        {!!this.state.dashboardId && (
-          <div>
-            <h2>Feedback form:</h2>
-            <FeedbackForm dashboardId={this.state.dashboardId} />
-          </div>
-        )}
+          {!!this.state.dashboardId && (
+            <div>
+              <h2>Feedback form:</h2>
+              <FeedbackForm dashboardId={this.state.dashboardId} />
+            </div>
+          )}
+        </div>
       </div>
     )
   }
